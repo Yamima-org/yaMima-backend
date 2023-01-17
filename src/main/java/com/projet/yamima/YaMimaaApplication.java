9ba9021a15d2bb77178201cd23027dad5909be55
zarganichaima <zarganichaima@gmail.com>
2023-01-17 18:58:11 +0100

    first commit backend

---
+++ src/main/java/com/projet/yamima/YaMimaaApplication.java
@@ -0,0 +1,13 @@
+package com.projet.yamima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaMimaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaMimaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet/yamima/YaMimaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.yamima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaMimaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
